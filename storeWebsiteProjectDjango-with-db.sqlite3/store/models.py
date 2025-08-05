b5eb6a6a1a6edea3fb1b01e5ae006b16b4e9ddfb
samira dorchalian <samiradorchalain1998@gmail.com>
2025-08-05 16:22:42 +0330

    add Product & Customer models

--- storeWebsiteProjectDjango-with-db.sqlite3/store/models.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/store/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
 
