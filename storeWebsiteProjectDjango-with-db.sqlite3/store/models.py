0327d3d999665b1f27a1cf54c87b7e67a5335a33
samira dorchalian <samiradorchalain1998@gmail.com>
2025-08-18 17:20:29 +0330

    use validators

--- storeWebsiteProjectDjango-with-db.sqlite3/store/models.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
@@ -24,7 +24,7 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField() # تعداد محصول موجود
+    inventory = models.IntegerField(validators=[MinValueValidator(0)]) # تعداد محصول موجود
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     discounts = models.ManyToManyField(Discount, blank=True)
