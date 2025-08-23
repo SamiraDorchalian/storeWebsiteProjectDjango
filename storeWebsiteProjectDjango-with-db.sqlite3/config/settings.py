61abfb0d89f258dfb40a53bae7a0e22337c0dba7
samira dorchalian <samiradorchalain1998@gmail.com>
2025-08-23 16:14:55 +0330

    add product_list & product_detail view and ProductSerializer

--- storeWebsiteProjectDjango-with-db.sqlite3/config/settings.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/config/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
     "debug_toolbar",
+    'rest_framework',
 
     # Local App
     'store',
