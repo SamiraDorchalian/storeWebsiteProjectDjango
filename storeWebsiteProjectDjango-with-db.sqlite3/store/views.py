a4aaad9698e54dbea8b124d96bb7919203e9106f
samira dorchalian <samiradorchalain1998@gmail.com>
2025-08-23 16:41:11 +0330

    use related field in serializer

--- storeWebsiteProjectDjango-with-db.sqlite3/store/views.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/store/views.py
@@ -9,12 +9,15 @@ from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    product_queryset = Product.objects.all()
+    product_queryset = Product.objects.select_related('category').all()
     serializer = ProductSerializer(product_queryset, many=True)
     return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
+    product = get_object_or_404(
+        Product.objects.select_related('category'), 
+        pk=id
+    )
     serializer = ProductSerializer(product)
     return Response(serializer.data)
