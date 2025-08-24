736531e87a374be6782ecd3f838346a6e9a83066
samira dorchalian <samiradorchalain1998@gmail.com>
2025-08-24 13:10:25 +0330

    add category_listView & category_detailView

--- storeWebsiteProjectDjango-with-db.sqlite3/store/urls.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/store/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
     path('products/<int:pk>/', views.product_detail, name='product-detail'),
+    path('categories/', views.category_list, name='category-list'),
     path('categories/<int:pk>/', views.category_detail, name='category-detail'),
 ]
--- storeWebsiteProjectDjango-with-db.sqlite3/store/views.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/store/views.py
@@ -1,5 +1,6 @@
+from itertools import count
 from django.shortcuts import get_object_or_404
-
+from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,7 +23,7 @@ def product_list(request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response('Everything is OK!')
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
@@ -43,9 +44,34 @@ def product_detail(request, pk):
             return Response({'error': 'There is some order including this product. Please remove them first.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-@api_view()
+
+@api_view(['GET','POST'])
+def category_list(request):
+    if request.method == 'GET':
+        categories_queryset = Category.objects.annotate(
+            products_count=Count('products')
+        ).all()
+        serializer = CategorySerializer(categories_queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def category_detail(request, pk):
-    category = get_object_or_404(Category, pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    category = get_object_or_404(Category.objects.annotate(products_count=Count('products')), pk=pk)
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if category.products.count() > 0:
+            return Response({'error': 'There is some products relating this category. Please remove them first.'})
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
