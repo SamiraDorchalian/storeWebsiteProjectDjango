3b6df8abde157dc4073fa83fa2076af041c587b6
samira dorchalian <samiradorchalain1998@gmail.com>
2025-08-25 13:57:11 +0330

    add CustomUser model

--- storeWebsiteProjectDjango-with-db.sqlite3/config/settings.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/config/settings.py
@@ -148,3 +148,5 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
+
+AUTH_USER_MODEL = 'core.CustomUser'
