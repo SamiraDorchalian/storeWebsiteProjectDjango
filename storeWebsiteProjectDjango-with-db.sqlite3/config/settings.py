04ca73e82fdba51e1f6bf6565d1f202d8fc94d5d
samira dorchalian <samiradorchalain1998@gmail.com>
2025-08-26 15:08:37 +0330

    How to get access token from refresh token

--- storeWebsiteProjectDjango-with-db.sqlite3/config/settings.py
+++ storeWebsiteProjectDjango-with-db.sqlite3/config/settings.py
@@ -156,7 +156,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT', ),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    # 'REFRESH_TOKEN_LIFETIME': timedelta(seconds=10)
 }
 
 DJOSER = {
