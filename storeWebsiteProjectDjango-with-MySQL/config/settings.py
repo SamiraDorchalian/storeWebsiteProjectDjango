43d6d5337f5f31d6e37ea9abdecb957dfd93c08c
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-02 20:48:57 +0330

    conect project to MySQL and add requirements.txt File

--- storeWebsiteProjectDjango-with-MySQL/config/settings.py
+++ storeWebsiteProjectDjango-with-MySQL/config/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'samira1998',
+        # --------------
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
