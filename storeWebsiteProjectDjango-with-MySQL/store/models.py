cb736e2355c6e1d17cb139e0deddf0f91c6e7627
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-02 19:18:59 +0330

    add some ForeignKey and add Category Model

--- storeWebsiteProjectDjango-with-MySQL/store/models.py
+++ storeWebsiteProjectDjango-with-MySQL/store/models.py
@@ -16,3 +16,33 @@ class Customer(models.Model):
     email = models.EmailField()
     phone_number = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
+
+
+class Order(models.Model):
+    ORDER_STATUS_PAID = 'p'
+    ORDER_STATUS_UNPAID = 'u'
+    ORDER_STATUS_CANCELED = 'c'
+    ORDER_STATUS = [
+        (ORDER_STATUS_PAID ,'Paid'),
+        (ORDER_STATUS_UNPAID ,'Unpaid'),
+        (ORDER_STATUS_CANCELED ,'Canceled'),
+    ]
+    # customer
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
+
+
+class Comment(models.Model):
+    COMMENT_STATUS_WAITING = 'W'
+    COMMENT_STATUS_APPROVED = 'a'
+    COMMENT_STATUS_NOT_APPROVED = 'na'
+    COMMENT_STATUS = [
+        (COMMENT_STATUS_WAITING,'Waiting'),
+        (COMMENT_STATUS_APPROVED,'Approved'),
+        (COMMENT_STATUS_NOT_APPROVED,'Not Approved'),
+    ]
+    # product
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_NOT_APPROVED)
