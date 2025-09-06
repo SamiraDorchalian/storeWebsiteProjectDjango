aec1dd936644bf3dffee6ae63a7402b36d0a841c
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-06 17:27:10 +0330

    use HyperlinkedRelatedField & add CategorySerializer - category_detailView

--- storeWebsiteProjectDjango-with-MySQL/store/urls.py
+++ storeWebsiteProjectDjango-with-MySQL/store/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='product_list'),
-    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('products/', views.product_list, name='product-list'),
+    path('products/<int:pk>/', views.product_detail, name='product-detail'),
+    path('categories/<int:pk>', views.category_detail, name='category-detail'),
 ]
--- storeWebsiteProjectDjango-with-MySQL/store/views.py
+++ storeWebsiteProjectDjango-with-MySQL/store/views.py
@@ -6,15 +6,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Category, Product
+from .serializers import ProductSerializer, CategorySerializer
 
 @api_view()
 def product_list(request):
     products_queryset = Product.objects.select_related('category').all()
-    serializer = ProductSerializer(products_queryset, many=True)
+    serializer = ProductSerializer(
+        products_queryset, 
+        many=True, 
+        context={'request': request}
+    )
     return Response(serializer.data)
-
 
 @api_view()
 def product_detail(request, pk):
@@ -22,5 +25,12 @@ def product_detail(request, pk):
         Product.objects.select_related('category'), 
         pk=pk
     )
-    serializer = ProductSerializer(product)
+    serializer = ProductSerializer(product, context={'request': request})
     return Response(serializer.data)
+
+@api_view()
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
+
