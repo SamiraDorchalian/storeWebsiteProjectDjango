0d00490b5201fea7e56ef6228527e369706c3940
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-06 17:09:19 +0330

    use related field in serializer

--- storeWebsiteProjectDjango-with-MySQL/store/serializers.py
+++ storeWebsiteProjectDjango-with-MySQL/store/serializers.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from rest_framework import serializers
 
-# DOLLORD_TO_RIALS = 500000
+from .models import Category
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -9,10 +9,7 @@ class ProductSerializer(serializers.Serializer):
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     unit_price_after_tax = serializers.SerializerMethodField()
     inventory = serializers.IntegerField()
-    # price_rials = serializers.SerializerMethodField()
-
-    # def get_price_rials(self, product):
-    #     return int(product.unit_price * DOLLORD_TO_RIALS)
+    category = serializers.StringRelatedField()
 
     def get_unit_price_after_tax(self, product):
         return round(product.unit_price * Decimal(1.09), 2)   
--- storeWebsiteProjectDjango-with-MySQL/store/views.py
+++ storeWebsiteProjectDjango-with-MySQL/store/views.py
@@ -11,13 +11,16 @@ from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
-    products_queryset = Product.objects.all()
+    products_queryset = Product.objects.select_related('category').all()
     serializer = ProductSerializer(products_queryset, many=True)
     return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+    product = get_object_or_404(
+        Product.objects.select_related('category'), 
+        pk=pk
+    )
     serializer = ProductSerializer(product)
     return Response(serializer.data)
