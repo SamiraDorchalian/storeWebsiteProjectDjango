54d438a8125672383ed82f98937730596df37bc8
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-06 15:06:33 +0330

    get and show data with api

--- storeWebsiteProjectDjango-with-MySQL/store/views.py
+++ storeWebsiteProjectDjango-with-MySQL/store/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view()
 def product_list(request):
     return Response('Hello')
 
+
 @api_view()
 def product_detail(request, pk):
-    return Response(pk)
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
