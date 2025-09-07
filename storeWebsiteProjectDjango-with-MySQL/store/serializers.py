01f3a137f72fab5b7bdb355795db35c8d38fb685
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-07 12:16:20 +0330

    RetrieveUpdateDestroyAPIView

--- storeWebsiteProjectDjango-with-MySQL/store/serializers.py
+++ storeWebsiteProjectDjango-with-MySQL/store/serializers.py
@@ -5,14 +5,15 @@ from django.utils.text import slugify
 from .models import Category, Product
 
 class CategorySerializer(serializers.ModelSerializer):
-    num_of_products = serializers.SerializerMethodField()
+    # num_of_products = serializers.SerializerMethodField()
+    num_of_products = serializers.IntegerField(source='products.count', read_only=True)
 
     class Meta:
         model = Category
         fields = ['id', 'title', 'description', 'num_of_products']
 
-    def get_num_of_products(self, category):
-        return category.products.count()
+    # def get_num_of_products(self, category):
+    #     return category.products.count()
 
 
 class ProductSerializer(serializers.ModelSerializer):
--- storeWebsiteProjectDjango-with-MySQL/store/views.py
+++ storeWebsiteProjectDjango-with-MySQL/store/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Category, Product
 from .serializers import ProductSerializer, CategorySerializer
@@ -19,24 +19,9 @@ class ProductList(ListCreateAPIView):
         return {'request': self.request}
 
 
-class ProductDetail(APIView):
-    def get(self, request, pk):
-        product = get_object_or_404(
-            Product.objects.select_related('category'), 
-            pk=pk,
-        )
-        serializer = ProductSerializer(product, context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self , request, pk):
-        product = get_object_or_404(
-            Product.objects.select_related('category'), 
-            pk=pk,
-        )
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.select_related('category').all()
     
     def delete(self, request, pk):
         product = get_object_or_404(
@@ -54,18 +39,9 @@ class CategoryList(ListCreateAPIView):
     queryset = Category.objects.prefetch_related('products').all()
 
 
-class CategoryDetail(APIView):
-    def get(self, request, pk):
-        category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
-        serializer = CategorySerializer(category, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class CategoryDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.prefetch_related('products').all()
     
     def delete(self, request, pk):
         category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
