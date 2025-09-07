8dde850a8bc6090b2302820fa3953551f9431ecb
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-07 12:32:13 +0330

    SimpleRouter in Routers

--- storeWebsiteProjectDjango-with-MySQL/store/urls.py
+++ storeWebsiteProjectDjango-with-MySQL/store/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 
 from . import views
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
-    path('categories/', views.CategoryList.as_view(), name='category-list'),
-    path('categories/<int:pk>', views.CategoryDetail.as_view(), name='category-detail'),
-]
+router = SimpleRouter()
+router.register('products', views.ProductViewSet, basename='product')
+router.register('categories', views.CategoryViewSet, basename='category')
+
+urlpatterns = router.urls
