02fff594b2aa9f5ac497bf9a295268111e9c11b4
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-09 14:49:43 +0330

    Request of type put for viewSets actions

--- storeWebsiteProjectDjango-with-MySQL/store/serializers.py
+++ storeWebsiteProjectDjango-with-MySQL/store/serializers.py
@@ -113,3 +113,4 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'user', 'birth_date']
+        read_only_fields = ['user']
