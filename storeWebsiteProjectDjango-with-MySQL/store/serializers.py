1aa73dfe56d80d0bdcafad3f6c6f896ede5d4c84
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-09 18:52:17 +0330

    order create serializer

--- storeWebsiteProjectDjango-with-MySQL/store/serializers.py
+++ storeWebsiteProjectDjango-with-MySQL/store/serializers.py
@@ -155,3 +155,31 @@ class OrderForAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'customer', 'status', 'datetime_created', 'items', ]
+
+
+class OrderCreateSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def validate_cart_id(self, cart_id):
+        # One Query
+        # try:
+        #     if Cart.objects.prefetch_related('items').get(id=cart_id).items.count() == 0:
+        #         raise serializers.ValidationError('Your Cart is empty! Please add some products to it first!')
+        # except Cart.DoesNotExist:
+        #     raise serializers.ValidationError('There is no cart with this cart id!')
+
+        # Two Query
+        if not Cart.objects.filter(id=cart_id).exists():
+            raise serializers.ValidationError('There is no cart with this cart id!')
+
+        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise serializers.ValidationError('Your Cart is empty! Please add some products to it first!')
+
+        return cart_id
+    
+    def save(self, **kwargs):
+        cart_id = self.validated_data['cart_id']
+        user_id = self.context['user_id']
+        customer = Customer.objects.get(user_id=user_id)
+
+        print(f'{cart_id=} {customer.user.first_name=}')
--- storeWebsiteProjectDjango-with-MySQL/store/views.py
+++ storeWebsiteProjectDjango-with-MySQL/store/views.py
@@ -15,7 +15,7 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny, D
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Cart, CartItem, Category, Customer, Order, OrderItem, Product, Comment
-from .serializers import AddCartItemSerializer, CartSerializer, CustomerSerializer, OrderForAdminSerializer, OrderSerializer, ProductSerializer, CategorySerializer, CommentSerializer, CartItemSerializer, UpdateCartItemSerializer
+from .serializers import AddCartItemSerializer, CartSerializer, CustomerSerializer, OrderCreateSerializer, OrderForAdminSerializer, OrderSerializer, ProductSerializer, CategorySerializer, CommentSerializer, CartItemSerializer, UpdateCartItemSerializer
 from .filters import ProductFilter
 from .paginations import DefaultPagination
 from .permissions import CustomDjangoModelPermissions, IsAdminOrReadOnly, SendPrivateEmailToCustomerPermission
@@ -138,7 +138,13 @@ class OrderViewSet(ModelViewSet):
         return queryset.filter(customer__user_id=user.id)
 
     def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return OrderCreateSerializer
+
         if self.request.user.is_staff:
             return OrderForAdminSerializer
         return OrderSerializer
+
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
 
