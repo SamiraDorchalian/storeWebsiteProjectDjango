d22f081d923d7b1de892bc33bbf2ff1dee5e1f8b
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-10 12:43:40 +0330

    View order after production

--- storeWebsiteProjectDjango-with-MySQL/store/views.py
+++ storeWebsiteProjectDjango-with-MySQL/store/views.py
@@ -119,7 +119,6 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    # serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
@@ -148,3 +147,13 @@ class OrderViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'user_id': self.request.user.id}
 
+    def create(self, request, *args, **kwargs):
+        create_order_serializer = OrderCreateSerializer(
+            data=request.data,
+            context={'user_id': self.request.user.id},
+        )
+        create_order_serializer.is_valid(raise_exception=True)
+        created_order = create_order_serializer.save()
+
+        serializer = OrderSerializer(created_order)
+        return Response(serializer.data)
