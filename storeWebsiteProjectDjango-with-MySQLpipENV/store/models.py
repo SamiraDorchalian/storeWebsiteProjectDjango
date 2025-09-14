a20219afd9318acd0fbdd5c14653df8ea39fcfdb
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-14 14:12:50 +0330

    add Order & Comment Model

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
@@ -15,8 +15,36 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
     phone_number = models.CharField(max_length=255)
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
 
 
+class Order(models.Model):
+    ORDER_STATUS_PAID = 'p' 
+    ORDER_STATUS_UNPAID = 'u' 
+    ORDER_STATUS_CANCELED = 'c' 
+    ORDER_STATUS = [
+        (ORDER_STATUS_PAID, 'Paid'),
+        (ORDER_STATUS_UNPAID, 'Unpaid'),
+        (ORDER_STATUS_CANCELED, 'Canceled'),
+    ]
+    # customer
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
 
-    
+
+class Comment(models.Model):
+    COMMENT_STATUS_WAITTING = 'w'
+    COMMENT_STATUS_APPROVED = 'A'
+    COMMENT_STATUS_NOT_APPROVED = 'na'
+    COMMENT_STATUS = [
+        (COMMENT_STATUS_WAITTING, 'Waitting'),
+        (COMMENT_STATUS_APPROVED, 'Approved'),
+        (COMMENT_STATUS_NOT_APPROVED, 'Not Approved'),
+    ]
+    # product
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_WAITTING)
+
+
