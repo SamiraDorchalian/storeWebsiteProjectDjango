3405e9b94a64d7190a2b0259d7ac2d2a8f7171cd
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-14 15:29:30 +0330

    add top_product Field in CategoryModel

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True)
+    top_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
 
 
 class Discount(models.Model):
