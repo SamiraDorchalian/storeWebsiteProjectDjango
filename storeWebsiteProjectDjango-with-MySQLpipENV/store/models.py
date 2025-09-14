bceb6fe12d2dcd847e6f159aaec7c0fe2b7967ce
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-14 16:48:45 +0330

    add factories and setup_fake_data Files

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
     slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
@@ -56,26 +56,26 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_items')
     quantity = models.PositiveSmallIntegerField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
         unique_together = [['order', 'product']]
 
 
 class Comment(models.Model):
-    COMMENT_STATUS_WAITTING = 'w'
+    COMMENT_STATUS_WAITING = 'w'
     COMMENT_STATUS_APPROVED = 'a'
     COMMENT_STATUS_NOT_APPROVED = 'na'
     COMMENT_STATUS = [
-        (COMMENT_STATUS_WAITTING, 'Waitting'),
-        (COMMENT_STATUS_APPROVED, 'Approved'),
-        (COMMENT_STATUS_NOT_APPROVED, 'Not Approved'),
+        (COMMENT_STATUS_WAITING , 'Waiting'),
+        (COMMENT_STATUS_APPROVED , 'Approved'),
+        (COMMENT_STATUS_NOT_APPROVED , 'Not Approved'),
     ]
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=255)
     body = models.TextField()
     datetime_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_WAITTING)
+    status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_WAITING)
 
 
 class Cart(models.Model):
