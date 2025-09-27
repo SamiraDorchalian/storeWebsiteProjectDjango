5cdbdb9d9ba58b8fdbc277b0e13b8e347f6df782
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-27 12:29:42 +0330

    add pagination

--- storeWebsiteProjectDjango-with-MySQLpipENV/config/settings.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/config/settings.py
@@ -169,8 +169,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     # 'PAGE_SIZE': 10,
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
