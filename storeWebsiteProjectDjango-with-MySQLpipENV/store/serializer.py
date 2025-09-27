17bfab1710eac778fd2ec8bbb5bc129b84c43b00
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-27 15:19:50 +0330

    Calculating the total price of the shopping cart and optimizing its query

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/serializer.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/serializer.py
@@ -72,9 +72,12 @@ class CartItemSerializer(serializers.ModelSerializer):
 class CartSerializer(serializers.ModelSerializer):
     # id = serializers.UUIDField(read_only=True)
     items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Cart
-        fields = ['id', 'items', ]
+        fields = ['id', 'items', 'total_price', ]
         read_only_fields = ['id', ]
 
+    def get_total_price(self, cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
--- storeWebsiteProjectDjango-with-MySQLpipENV/store/views.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/views.py
@@ -73,4 +73,4 @@ class CartViewSet(CreateModelMixin,
                    RetrieveModelMixin,
                    GenericViewSet):
     serializer_class = CartSerializer
-    queryset = Cart.objects.all()
+    queryset = Cart.objects.prefetch_related('items__product').all()
