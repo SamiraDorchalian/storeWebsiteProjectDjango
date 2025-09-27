dd28e25e04bc55f2bdc57dff03a850a1436a3cde
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-27 13:57:06 +0330

    Show shopping cart items in cart Detail

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/serializer.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/serializer.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from django.utils.text import slugify
 from rest_framework import serializers
 
-from .models import Cart, Category, Product, Comment
+from .models import Cart, CartItem, Category, Product, Comment
 
 class CategorySerializer(serializers.ModelSerializer):
     # num_of_products = serializers.SerializerMethodField()
@@ -51,11 +51,18 @@ class CommentSerializer(serializers.ModelSerializer):
         return Comment.objects.create(product_id=product_id, **validated_data)
 
 
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity']
+
+
 class CartSerializer(serializers.ModelSerializer):
     # id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True)
 
     class Meta:
         model = Cart
-        fields = ['id', ]
-        read_only_fields = ['id', ]
+        fields = ['id', 'items', ]
+        read_only_fields = ['id', 'items', ]
 
