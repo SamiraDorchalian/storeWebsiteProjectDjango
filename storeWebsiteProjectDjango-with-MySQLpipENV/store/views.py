98b1944ef418f685b7aec77908c4cbd4efc3de7e
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-28 17:02:03 +0330

    permission

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/views.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/views.py
@@ -13,6 +13,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Cart, CartItem, Category, Customer, Product, Comment
@@ -100,8 +101,9 @@ class CartViewSet(CreateModelMixin,
 class CustomerViewSet(ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
+    permission_classes = [IsAdminUser]
 
-    @action(detail=False, methods=['GET', 'PUT', ])
+    @action(detail=False, methods=['GET', 'PUT', ], permission_classes=[IsAuthenticated])
     def me(self, request):
         user_id = request.user.id
         customer = Customer.objects.get(user_id=user_id)
