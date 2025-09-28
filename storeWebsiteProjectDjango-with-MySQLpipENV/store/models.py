833a3d67d0a395731b99af1269c33412bd6d55d9
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-28 15:09:06 +0330

    signals

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from uuid import uuid4
 
 class Category(models.Model):
