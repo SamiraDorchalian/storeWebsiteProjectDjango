a5a3e074921b8a581e09eaffff808d5450ed1d7f
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-09-29 12:52:14 +0330

    transaction

--- storeWebsiteProjectDjango-with-MySQLpipENV/store/serializer.py
+++ storeWebsiteProjectDjango-with-MySQLpipENV/store/serializer.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from django.utils.text import slugify
+from django.db import transaction
 from rest_framework import serializers
 
 from .models import Cart, CartItem, Category, Customer, Order, OrderItem, Product, Comment
@@ -178,37 +179,38 @@ class OrderCreateSerializer(serializers.Serializer):
         return cart_id
 
     def save(self, **kwargs):
-        cart_id = self.validated_data['cart_id']
-        user_id = self.context['user_id']
-        customer = Customer.objects.get(user_id=user_id)
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+            user_id = self.context['user_id']
+            customer = Customer.objects.get(user_id=user_id)
 
-        order = Order()
-        order.customer = customer
-        order.save()
+            order = Order()
+            order.customer = customer
+            order.save()
 
-        cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
+            cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
 
-        order_items = [
-            OrderItem(
-                order=order,
-                product=cart_item.product,
-                unit_price=cart_item.product.unit_price,
-                quantity=cart_item.quantity,
-            ) for cart_item in cart_items
-        ]
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=cart_item.product,
+                    unit_price=cart_item.product.unit_price,
+                    quantity=cart_item.quantity,
+                ) for cart_item in cart_items
+            ]
 
-        # order_items = list()
-        # for cart_item in cart_items:
-        #     order_item = OrderItem()
-        #     order_item.order = order
-        #     order_item.product_id = cart_item.product_id
-        #     order_item.unit_price = cart_item.product.unit_price
-        #     order_item.quantity = cart_item.quantity
-    
-        #     order_items.append(order_item)
+            # order_items = list()
+            # for cart_item in cart_items:
+            #     order_item = OrderItem()
+            #     order_item.order = order
+            #     order_item.product_id = cart_item.product_id
+            #     order_item.unit_price = cart_item.product.unit_price
+            #     order_item.quantity = cart_item.quantity
+        
+            #     order_items.append(order_item)
 
-        OrderItem.objects.bulk_create(order_items)
+            OrderItem.objects.bulk_create(order_items)
 
-        Cart.objects.get(id=cart_id).delete()
+            Cart.objects.get(id=cart_id).delete()
 
-        return order
+            return order
